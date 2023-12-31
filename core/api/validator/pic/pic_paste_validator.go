97474ad10cdeb07e208996efe9f5708127b92ad6
cmx_star <316785038@qq.com>
2023-12-31 11:07:30 +0800

    添加多个自定义验证方法

--- core/api/validator/pic/pic_paste_validator.go
+++ core/api/validator/pic/pic_paste_validator.go
@@ -12,15 +12,15 @@ import (
 
 type Request struct {
 	BatchNo          string  `json:"batch_no" form:"batch_no" validate:"required" comment:"批次号"`
-	OriginalImageUrl string  `json:"original_image_url" form:"original_image_url" validate:"required" comment:"底图"`
+	OriginalImageUrl string  `json:"original_image_url" form:"original_image_url" validate:"required,uri" comment:"底图"`
 	CompressFileUrl  string  `json:"compress_file_url" form:"compress_file_url" validate:"required" comment:"贴图压缩包文件地址"`
 	X                uint    `json:"x" form:"x" validate:"required" comment:"x轴坐标"`
 	Y                uint    `json:"y" form:"y" validate:"required" comment:"y轴坐标"`
-	R                uint    `json:"r" form:"r" validate:"isSquareR" comment:"半径"`
+	R                uint    `json:"r" form:"r" validate:"isRequiredR" comment:"半径"`
 	Type             uint    `json:"type" form:"type" validate:"required" comment:"贴图放大/缩小"`
 	Multiple         float32 `json:"multiple" form:"multiple" validate:"required" comment:"贴图放大缩小值"`
-	IsSquare         uint    `json:"is_square" form:"is_square" validate:"required" comment:"是否方形"`
-	SideLength       uint    `json:"side_length" form:"side_length" validate:"required" comment:"方形的变长"`
+	IsSquare         uint    `json:"is_square" form:"is_square" comment:"是否方形"`
+	SideLength       uint    `json:"side_length" form:"side_length" validate:"isRequiredSideLength" comment:"方形的变长"`
 }
 
 func ValidateRequest(c *gin.Context) (*Request, error) {
@@ -38,8 +38,8 @@ func ValidateRequest(c *gin.Context) (*Request, error) {
 		return fld.Tag.Get("comment")
 	})
 
-	// 注册自定义验证方法
-	validatorInstance.RegisterValidation("isSquareR", func(fl validator.FieldLevel) bool {
+	//圆形贴图的半径值验证
+	validatorInstance.RegisterValidation("isRequiredR", func(fl validator.FieldLevel) bool {
 		// 获取结构体中的字段
 		isSquareField := fl.Parent().FieldByName("IsSquare")
 		rField := fl.Parent().FieldByName("R")
@@ -47,9 +47,25 @@ func ValidateRequest(c *gin.Context) (*Request, error) {
 		// 获取 IsSquare 和 R 字段的值
 		isSquareValue := isSquareField.Uint()
 		rValue := rField.Uint()
+		if isSquareValue == 0 && rValue == 0 {
+			return false
+		}
+		return true
+	})
 
-		// 如果 IsSquare 的值等于 0，则要求 R 参数必传
-		return isSquareValue == 0 && rValue > 0
+	// 方形贴图的边长值验证
+	validatorInstance.RegisterValidation("isRequiredSideLength", func(fl validator.FieldLevel) bool {
+		// 获取结构体中的字段
+		isSquareField := fl.Parent().FieldByName("IsSquare")
+		sideLength := fl.Parent().FieldByName("SideLength")
+
+		// 获取 IsSquare 和 R 字段的值
+		isSquareValue := isSquareField.Uint()
+		sideLengthValue := sideLength.Uint()
+		if isSquareValue == 1 && sideLengthValue == 0 {
+			return false
+		}
+		return true
 	})
 
 	// 进行进一步的验证
@@ -58,8 +74,10 @@ func ValidateRequest(c *gin.Context) (*Request, error) {
 		errs := err.(validator.ValidationErrors)
 		var sliceErrs []string
 		for _, e := range errs {
-			if e.Tag() == "isSquareR" {
-				sliceErrs = append(sliceErrs, "自定义错误消息")
+			if e.Tag() == "isRequiredSideLength" {
+				sliceErrs = append(sliceErrs, "方形贴图不能没有边长值")
+			} else if e.Tag() == "isRequiredR" {
+				sliceErrs = append(sliceErrs, "圆形贴图不能没有半径值")
 			} else {
 				sliceErrs = append(sliceErrs, e.Translate(utTrans))
 			}
