4cb6bc910593860f5ed04497ef2fe3d52ab0e1f0
cmx_star <316785038@qq.com>
2025-01-12 20:35:29 +0800

    调整readme文件

--- alitools/models/TimeNormal.go
+++ alitools/models/TimeNormal.go
@@ -13,17 +13,19 @@ type TimeNormal time.Time
 
 // MarshalJSON 用于将 TimeNormal 类型转换为 JSON 字节数组的方法。在该方法中，时间被格式化为指定的字符串格式，并以 JSON 字符串的形式返回。
 func (t TimeNormal) MarshalJSON() ([]byte, error) {
+	// 检查时间是否为零值
+
 	b := make([]byte, 0, len(timeFormat)+2)
 	b = append(b, '"')
 	b = time.Time(t).AppendFormat(b, timeFormat)
 	b = append(b, '"')
+
 	return b, nil
 }
 
 // UnmarshalJSON 用于从 JSON 字节数组解析并设置 TimeNormal 类型的值。在该方法中，通过 time.ParseInLocation 解析 JSON 字符串，并将其转换为 TimeNormal 类型
 func (t *TimeNormal) UnmarshalJSON(data []byte) (err error) {
 
-	fmt.Println("data是", string(data))
 	now, err := time.ParseInLocation(`"`+timeFormat+`"`, string(data), time.Local)
 	*t = TimeNormal(now)
 	return
@@ -48,6 +50,7 @@ func (t TimeNormal) Value() (driver.Value, error) {
 		return nil, nil
 	}
 	return ti, nil
+	//return ti.Format(timeFormat), nil
 }
 
 // Scan 实现 database/sql.Scanner 接口的方法，用于从数据库扫描操作中读取时间值，并设置为 TimeNormal 类型。
@@ -58,4 +61,13 @@ func (t *TimeNormal) Scan(v interface{}) error {
 		return nil
 	}
 	return fmt.Errorf("can not convert %v to timestamp", v)
+}
+
+// ParseTimeString 将年月日时分秒的字符串转换为 TimeNormal 类型。
+func ParseTimeString(s string) (TimeNormal, error) {
+	t, err := time.Parse(timeFormat, s)
+	if err != nil {
+		return TimeNormal(time.Time{}), err
+	}
+	return TimeNormal(t), nil
 }
